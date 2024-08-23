9bcff0c977d42689a304e171d2f78ddf983d9a45
Willy <willy.it.wonka@outlook.com>
2024-08-23 18:46:40 +0200

    Fix notes.py

--- notes.py
+++ notes.py
@@ -5,9 +5,11 @@ with open('notes.json', 'r+') as file:
     data = json.load(file)
     
     # Update id and book_id.
-    for index, item in enumerate(data, start=4):
-        item['id'] = index
-        item['book_id'] = index
+    id_start = 4
+    book_id_start = 22
+    for index, item in enumerate(data):
+        item['id'] = id_start + index
+        item['book_id'] = book_id_start + index
 
     file.seek(0) # Move to the beginning of the file to overwrite it.
     json.dump(data, file, indent=2, ensure_ascii=False) # Save the updated data back to the file.
