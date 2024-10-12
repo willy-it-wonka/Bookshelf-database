cc4b765050a50fb87d39ea1ecd3a6213ad608a24
Willy <willy.it.wonka@outlook.com>
2024-10-12 21:52:39 +0200

    Extend notes.py by replaceing \n with \\n in notes.json

--- notes.py
+++ notes.py
@@ -10,6 +10,10 @@ with open('notes.json', 'r+') as file:
     for index, note in enumerate(data):
         note['id'] = id_start + index
         note['book_id'] = book_id_start + index
+        # Replace "\n" with "\\n" in the 'content' field. 
+        # Comment out the following 2 lines if you will import the JSON file and not using it in a SQL script.
+        if 'content' in note:
+            note['content'] = note['content'].replace('\n', '\\n')
 
     file.seek(0) # Move to the beginning of the file to overwrite it.
     json.dump(data, file, indent=2, ensure_ascii=False) # Save the updated data back to the file.
